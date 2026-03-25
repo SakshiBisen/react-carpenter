104b41ac2178ab3c2bb02f360f92710adf32d7eb
Sakshi Bisen <sakshibisen108@gmail.com>
2026-03-25 12:57:00 +0530

    first commit

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,17 +1,17 @@
 function Hero() {
   return (
     <section
-      className="h-screen bg-cover bg-center relative flex items-center justify-center"
+      className="h-screen w-full overflow-hidden bg-cover bg-center relative flex items-center justify-center"
       style={{
         backgroundImage: `url("https://images.unsplash.com/photo-1503387762-592deb58ef4e?q=80&w=1920&auto=format&fit=crop")`
       }}
     >
       {/* Overlay */}
-      <div className=" absolute inset-0 bg-black/50"></div>
+      <div className="absolute inset-0 bg-black/50"></div>
 
       {/* Content */}
-      <div className="relative z-10 max-w-4xl mx-auto text-center text-white px-6">
-        <h1 className="text-5xl md:text-6xl font-bold mb-6">
+      <div className="relative z-10 max-w-4xl w-full mx-auto text-center text-white px-4 sm:px-6">
+        <h1 className="text-4xl md:text-6xl font-bold mb-6">
           Premium Custom Furniture in Jodhpur.
         </h1>
 
@@ -29,5 +29,4 @@ function Hero() {
     </section>
   );
 }
-
 export default Hero;
